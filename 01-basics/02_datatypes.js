f6db056a9182e8eaeee1528f74f42b64639810a4
Mohammed Israhar <mk9743900@gmail.com>
2024-01-08 20:18:27 +0530

    config done

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,8 @@
+let score = null
+let num = 4545
+let string = "khan"
+let bool = true
+let isLoggedIn = 0 //0 is false, 1 is true
+let booleanIsLoggedIn= Boolean(isLoggedIn)//by using Boolean keyord we convert any thing to string
+console.log(booleanIsLoggedIn );
+console.table([score,num,string,bool,booleanIsLoggedIn])
