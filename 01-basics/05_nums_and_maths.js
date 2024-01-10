22c230137ab2ffe0cc5a9b341c44f14bba35fa0a
Mohammed Israhar <mk9743900@gmail.com>
2024-01-10 18:02:24 +0530

    done

---
+++ 01-basics/05_nums_and_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(typeof(balance));
+
+const otherNumberr = 500.2155
+console.log(otherNumberr.toFixed(1))
+
+console.log(otherNumberr.toPrecision(4));
+
+console.log(Math.random());
+console.log(Math.round(33.45));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
