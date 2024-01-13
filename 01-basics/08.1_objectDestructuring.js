dc1033e6150c0e0621d9ea637e6f5e5b2e1c2728
Mk9743900 <143392275+Mk9743900@users.noreply.github.com>
2024-01-13 12:00:46 +0000

    config done

---
+++ 01-basics/08.1_objectDestructuring.js
@@ -0,0 +1,10 @@
+//object Destructureing
+
+const course = {
+    name : "js in hindi",
+    price : 99,
+    courseInstructor : "hitesh"
+}
+
+const {courseInstructor} = course
+console.log(course); 
