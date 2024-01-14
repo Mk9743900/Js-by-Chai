43a0aa1a5de1f384d74030af3163ce0c7705ec5d
Mohammed Israhar <mk9743900@gmail.com>
2024-01-14 10:53:18 +0530

    done

--- 04-arrowFuntion/01_arrowFuntion_this.js
+++ 04-arrowFuntion/01_arrowFuntion_this.js
@@ -16,3 +16,9 @@ function one() {
     console.log(this.userName); //we can use 'this' inside a funtion.
 }
 one()
+
+//Arrow funtion
+const addTwo = (num1,num2) =>{return num1+num2}//if we use {} we have to return
+const addTwo2 = (num1,num2) =>(num1+num2)// if we use () no need to return.
+console.log(addTwo(2,3))
+console.log(addTwo2(3,3))
