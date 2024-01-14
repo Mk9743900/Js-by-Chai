07785183a88724a7088bdd6da5984b2b104587ea
Mohammed Israhar <mk9743900@gmail.com>
2024-01-14 12:43:03 +0530

    config done

---
+++ 05-control_flow/one.js
@@ -0,0 +1,14 @@
+// if 
+const isUserLoggedIn = true 
+const debitCard = true
+if (isUserLoggedIn && debitCard) {
+    console.log("Works ");
+}
+
+const balance = 1000
+
+if (balance < 500) {
+    console.log("less then 500");
+} else if (balance > 500) {
+    console.log("more then 500");
+}
