ccaaa4b44977445c3b5481be7caa2a0568ade4af
Mohammed Israhar <mk9743900@gmail.com>
2024-01-16 07:47:18 +0530

    done

---
+++ 06_iterations/01_loops.js
@@ -0,0 +1,17 @@
+//for
+
+// for(let i = 0 ;i < 11;i++){
+//     const element = i;
+//     console.log(element);
+// }
+
+for (let i = 0; i <= 10 ; i++) {
+    console.log(`outer loop ${i}`);
+    for (let j = 0; j <= 10; j++) {  
+        console.log(`inner loop ${j}`);
+    }
+    
+}
+
+//break && continue
+
