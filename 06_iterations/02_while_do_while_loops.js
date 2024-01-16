e0985885206c3b7a7725784436f0f21dc02e7e6d
Mohammed Israhar <mk9743900@gmail.com>
2024-01-16 07:59:02 +0530

    config done

---
+++ 06_iterations/02_while_do_while_loops.js
@@ -0,0 +1,19 @@
+let index = 0
+while (index <=10) {
+    // console.log(`value of index ${index}`);
+    index=index+2
+}
+//
+let myArray = ["flash","batman","superman"]
+let arr = 0
+while (arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+//doWhileLoop
+let score = 1
+do{
+    console.log(`score ${score}`);
+    score++
+} while(score <= 10)
+
